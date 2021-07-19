78b531e1d8747cf91b3319d51bddd781e15bbf3b
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-07-19 18:31:21 +0900

    make pipline state

--- ARKitExamples/Samples/HumanStencil/YCbCrToRGBConverter.swift
+++ ARKitExamples/Samples/HumanStencil/YCbCrToRGBConverter.swift
@@ -31,23 +31,85 @@ class YCbCrToRGBConverter {
     ]
     private var imagePlaneVertexBuffer: MTLBuffer!
     
+    private lazy var width = view.currentDrawable!.texture.width
+    private lazy var height = view.currentDrawable!.texture.height
+
     private(set) var sceneColorTexture: MTLTexture!
     private(set) var sceneDepthTexture: MTLTexture!
 
     let session: ARSession
 
     init(_ device: MTLDevice, session: ARSession, view: MTKView) {
-        func initVertexBuffer() {
+        func createVertexBuffer() {
             let imagePlaneVertexDataCount = kImagePlaneVertexData.count * MemoryLayout<Float>.size
             imagePlaneVertexBuffer = device.makeBuffer(bytes: kImagePlaneVertexData, length: imagePlaneVertexDataCount, options: [])
             imagePlaneVertexBuffer.label = "ImagePlaneVertexBuffer"
+        }
+        func createTextures() {
+            let width = view.currentDrawable!.texture.width
+            let height = view.currentDrawable!.texture.height
+
+            let colorDesc = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: view.colorPixelFormat,
+                                                                 width: width, height: height, mipmapped: false)
+            colorDesc.usage = MTLTextureUsage(rawValue: MTLTextureUsage.renderTarget.rawValue | MTLTextureUsage.shaderRead.rawValue)
+
+            sceneColorTexture =  device.makeTexture(descriptor: colorDesc)
+
+            let depthDesc = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: view.depthStencilPixelFormat,
+                                                                 width: width, height: height, mipmapped: false)
+            depthDesc.usage = MTLTextureUsage(rawValue: MTLTextureUsage.renderTarget.rawValue | MTLTextureUsage.shaderRead.rawValue)
+
+            sceneDepthTexture = device.makeTexture(descriptor: depthDesc)
+        }
+        func createPipelineState() {
+            let defaultLibrary = device.makeDefaultLibrary()!
+            let capturedImageVertexFunction = defaultLibrary.makeFunction(name: "capturedImageVertexTransform")!
+            let capturedImageFragmentFunction = defaultLibrary.makeFunction(name: "capturedImageFragmentShader")!
+
+            let imagePlaneVertexDescriptor = MTLVertexDescriptor()
+            
+            imagePlaneVertexDescriptor.attributes[0].format = .float2
+            imagePlaneVertexDescriptor.attributes[0].offset = 0
+            imagePlaneVertexDescriptor.attributes[0].bufferIndex = Int(kBufferIndexMeshPositions.rawValue)
+            
+            imagePlaneVertexDescriptor.attributes[1].format = .float2
+            imagePlaneVertexDescriptor.attributes[1].offset = 8
+            imagePlaneVertexDescriptor.attributes[1].bufferIndex = Int(kBufferIndexMeshPositions.rawValue)
+            
+            imagePlaneVertexDescriptor.layouts[0].stride = 16
+            imagePlaneVertexDescriptor.layouts[0].stepRate = 1
+            imagePlaneVertexDescriptor.layouts[0].stepFunction = .perVertex
+
+            let capturedImagePipelineStateDescriptor = MTLRenderPipelineDescriptor()
+            capturedImagePipelineStateDescriptor.label = "MyCapturedImagePipeline"
+            capturedImagePipelineStateDescriptor.sampleCount = view.sampleCount
+            capturedImagePipelineStateDescriptor.vertexFunction = capturedImageVertexFunction
+            capturedImagePipelineStateDescriptor.fragmentFunction = capturedImageFragmentFunction
+            capturedImagePipelineStateDescriptor.vertexDescriptor = imagePlaneVertexDescriptor
+            capturedImagePipelineStateDescriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat
+            capturedImagePipelineStateDescriptor.depthAttachmentPixelFormat = view.depthStencilPixelFormat
+            
+            do {
+                try capturedImagePipelineState = device.makeRenderPipelineState(descriptor: capturedImagePipelineStateDescriptor)
+            } catch let error {
+                print("Failed to created captured image pipeline state, error \(error)")
+            }
+        }
+        func createDepthState() {
+            let capturedImageDepthStateDescriptor = MTLDepthStencilDescriptor()
+            capturedImageDepthStateDescriptor.depthCompareFunction = .always
+            capturedImageDepthStateDescriptor.isDepthWriteEnabled = false
+            capturedImageDepthState = device.makeDepthStencilState(descriptor: capturedImageDepthStateDescriptor)
         }
 
         self.session = session
         self.device = device
         self.view = view
 
-        initVertexBuffer()
+        createVertexBuffer()
+        createTextures()
+        createPipelineState()
+        createDepthState()
     }
     
     func compositeImagesWithEncoder(_ commandBuffer: MTLCommandBuffer, textureY: CVMetalTexture, textureCbCr: CVMetalTexture) {
@@ -89,5 +151,7 @@ class YCbCrToRGBConverter {
         renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
         
         renderEncoder.popDebugGroup()
+        
+        renderEncoder.endEncoding()
     }
 }
