cbc79bb79b48597d5b1ccbe12c8a21d9e7699cc5
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-07-20 18:37:01 +0900

    fix buffer

--- ARKitExamples/Samples/HumanStencil/HumanStencilRenderer.swift
+++ ARKitExamples/Samples/HumanStencil/HumanStencilRenderer.swift
@@ -13,27 +13,32 @@ import CoreImage
 class HumanStencilRenderer {
     private let device: MTLDevice
     private var renderPipeline: MTLRenderPipelineState!
+    private var imagePlaneVertexBuffer: MTLBuffer!
+    private var scenePlaneVertexBuffer: MTLBuffer!
 
-    private var vertextBuffer: MTLBuffer!
-    private let vertexData: [Float] = [
-        -1, -1, 0, 1,
-         1, -1, 0, 1,
-        -1,  1, 0, 1,
-         1,  1, 0, 1,
+    let kImagePlaneVertexData: [Float] = [
+        -1.0, -1.0, 0.0, 1.0,
+        1.0, -1.0, 1.0, 1.0,
+        -1.0, 1.0, 0.0, 0.0,
+        1.0, 1.0, 1.0, 0.0
     ]
 
     init(device: MTLDevice) {
         func buildPipeline() {
             guard let library = device.makeDefaultLibrary() else {fatalError()}
             let descriptor = MTLRenderPipelineDescriptor()
-            descriptor.vertexFunction = library.makeFunction(name: "simpleVertexShader")
+            descriptor.vertexFunction = library.makeFunction(name: "compositeImageVertexTransform")
             descriptor.fragmentFunction = library.makeFunction(name: "compositeImageFragmentShader")
             descriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
             renderPipeline = try! device.makeRenderPipelineState(descriptor: descriptor)
         }
         func buildBuffers() {
-            let size = vertexData.count * MemoryLayout<Float>.size
-            vertextBuffer = device.makeBuffer(bytes: vertexData, length: size)
+            let imagePlaneVertexDataCount = kImagePlaneVertexData.count * MemoryLayout<Float>.size
+            imagePlaneVertexBuffer = device.makeBuffer(bytes: kImagePlaneVertexData, length: imagePlaneVertexDataCount, options: [])
+            imagePlaneVertexBuffer.label = "ImagePlaneVertexBuffer"
+
+            scenePlaneVertexBuffer = device.makeBuffer(bytes: kImagePlaneVertexData, length: imagePlaneVertexDataCount, options: [])
+            scenePlaneVertexBuffer.label = "ScenePlaneVertexBuffer"
         }
 
         self.device = device
@@ -52,7 +57,8 @@ class HumanStencilRenderer {
 
         
         renderEncoder.setRenderPipelineState(renderPipeline)
-        renderEncoder.setVertexBuffer(vertextBuffer, offset: 0, index: 0)
+        renderEncoder.setVertexBuffer(imagePlaneVertexBuffer, offset: 0, index: 0)
+        renderEncoder.setVertexBuffer(scenePlaneVertexBuffer, offset: 0, index: 1)
         renderEncoder.setFragmentTexture(cameraTexture, index: 0)
         renderEncoder.setFragmentTexture(textureY, index: 1)
         renderEncoder.setFragmentTexture(textureCbCr, index: 2)
